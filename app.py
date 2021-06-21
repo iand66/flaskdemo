41394d72808465ca2660acab3080663ae0a70349
iand66 <idavies66@gmail.com>
2021-06-21 17:59:54 +0100

    Incremental Update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -25,11 +25,16 @@ def demo():
     pizza = ['Pepperoni', 'Cheese', 'Mushroom']
     return render_template('demo.html', data=data, pizza=pizza)
 
-app.route('/username', methods=['GET','POST'])
+@app.route('/username', methods=['GET','POST'])
 def username():
     name = None
     form = NameForm()
-    return render_template('name.html')
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Success")
+        
+    return render_template('name.html', name=name, form=form)
 
 @app.errorhandler(404)
 def page_not_found(e):
